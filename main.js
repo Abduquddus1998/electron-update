5fa5b87d5f4c8b4d8b710c8cc6a87a18a45ad5b0
Abduquddus Azizov <abduquddus.08@mail.ru>
2023-02-23 02:09:18 +0500

    1.1.8

--- main.js
+++ main.js
@@ -1,6 +1,5 @@
-const { app, BrowserWindow, ipcMain, dialog} = require('electron');
+const { app, BrowserWindow, dialog} = require('electron');
 const { autoUpdater } = require('electron-updater');
-const log = require('electron-log');
 let mainWindow;
 
 
@@ -14,19 +13,12 @@ function createWindow () {
     });
     mainWindow.loadFile('index.html');
 
-
-    log.info('main window created annd checking for update');
     autoUpdater.checkForUpdates();
 }
 
 app.on('ready', () => {
     createWindow();
 });
-
-function sendStatusToWindow(text) {
-    log.info(text);
-    mainWindow.webContents.send('message', text);
-}
 
 app.on('window-all-closed', function () {
     if (process.platform !== 'darwin') {
@@ -41,10 +33,7 @@ app.on('activate', function () {
 });
 
 autoUpdater.on('update-available', (_event, releaseNotes, releaseName) => {
-    log.info('update-available listener', releaseNotes, releaseName, _event);
-    sendStatusToWindow('Update available. sendStatusToWindow');
 
-    mainWindow.webContents.send('update_available');
     const dialogOpts = {
         type: 'info',
         buttons: ['Ok'],
@@ -59,14 +48,22 @@ autoUpdater.on('download-progress', (progressObj) => {
     let log_message = "Download speed: " + progressObj.bytesPerSecond;
     log_message = log_message + ' - Downloaded ' + progressObj.percent + '%';
     log_message = log_message + ' (' + progressObj.transferred + "/" + progressObj.total + ')';
-    sendStatusToWindow(log_message);
+
+    const dialogOpts = {
+        type: 'info',
+        buttons: ['Ok'],
+        title: 'download-progress',
+        message: "message",
+        detail: log_message
+    };
+
+    dialog.showMessageBox(dialogOpts);
 })
 
 autoUpdater.on('update-downloaded', (_event, releaseNotes, releaseName) => {
     // log.info('update-downloaded', releaseNotes, releaseName, _event);
     // sendStatusToWindow('Update downloaded sendStatusToWindow');
 
-    mainWindow.webContents.send('update_downloaded');
     const dialogOpts = {
         type: 'info',
         buttons: ['Restart', 'Later'],
@@ -77,7 +74,6 @@ autoUpdater.on('update-downloaded', (_event, releaseNotes, releaseName) => {
     }
 
     dialog.showMessageBox(dialogOpts).then((returnValue) => {
-        log.info('update-downloaded showMessageBox', returnValue, releaseNotes, releaseName, _event);
         autoUpdater.quitAndInstall()
 
         if (returnValue.response === 0) {
